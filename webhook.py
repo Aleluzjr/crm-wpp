4313c550c326e494148d04ddb002d4861157cd1d
Alexandre Luz <amsljr@hotmail.com>
2025-01-28 21:16:54 -0300

    Adiciona suporte para application/x-www-form-urlencoded

--- webhook.py
+++ webhook.py
@@ -16,26 +16,23 @@ logging.basicConfig(
 def webhook():
     try:
         # Verificar se o Content-Type é application/json
-        if request.content_type != 'application/json':
-            logging.error(f"Unsupported Media Type: {request.content_type}")
-            return jsonify({"error": "Invalid content type, expected application/json"}), 415
-
-        # Validação do método da requisição
-        if request.method == 'GET':
-            # Resposta para validação da URL pela plataforma
-            logging.info("GET request received. Webhook is live!")
-            return jsonify({"message": "Webhook is live!"}), 200
-
-        elif request.method == 'POST':
-            # Receber os dados no formato JSON
+        if request.content_type == 'application/json':
             data = request.get_json()
-            if not data:
-                logging.error("No JSON data received")
-                return jsonify({"error": "No JSON received"}), 400
+        elif request.content_type == 'application/x-www-form-urlencoded':
+            # Quando o tipo for 'application/x-www-form-urlencoded', processa os dados
+            data = request.form.to_dict()
+        else:
+            logging.error(f"Unsupported Media Type: {request.content_type}")
+            return jsonify({"error": "Invalid content type, expected application/json or application/x-www-form-urlencoded"}), 415
 
-            # Registrar os dados no arquivo de log
-            logging.info(f"Received data: {data}")
-            return jsonify({"message": "Data received successfully"}), 200
+        # Se não houver dados, retorna erro
+        if not data:
+            logging.error("No data received")
+            return jsonify({"error": "No data received"}), 400
+
+        # Registrar os dados no arquivo de log
+        logging.info(f"Received data: {data}")
+        return jsonify({"message": "Data received successfully"}), 200
 
     except Exception as e:
         # Captura erros e registra no log
